1b829afcebac1a6f1148cd6a4f4d61c5613807b0
AntonM <anton_b_mitkov@abv.bg>
2017-10-13 01:34:11 +0100

    All Tasks Working

---
+++ Body.cpp
@@ -0,0 +1,34 @@
+#include "Body.h"
+
+ Body::Body()
+ {}
+
+ Body ::~Body()
+ {}
+
+ /* TRANSFORMATION METHODS */
+ void Body::translate(const glm::vec3 & vect) {
+	 m_pos = m_pos + vect;
+	 m_mesh.translate(vect);
+	
+}
+
+ void Body::rotate(float angle, const glm::vec3 & vect) {
+	 m_mesh.rotate(angle, vect);
+	
+}
+
+ void Body::scale(const glm::vec3 & vect) {
+	 m_mesh.scale(vect);
+	
+}
+
+glm::vec3 Body::applyForces(glm::vec3 pos, glm::vec3 vel) {
+	 glm::vec3 fAccumulator = glm::vec3(0.0f);
+
+	 for (auto &f : m_forces) 
+	 {
+		 fAccumulator += f->apply(getMass(), pos, vel);
+	 }
+	 return fAccumulator / getMass();
+}
